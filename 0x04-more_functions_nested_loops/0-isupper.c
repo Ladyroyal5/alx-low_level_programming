1e138cc555a1b1062590ef54e1756f39eb4b61d6
lilian <ladyroyal.ellalily@gmail.com>
2022-10-20 17:38:40 -0700

    task first

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -5,9 +5,9 @@
  * @c: The character to be checked.
  * Return: 1 if character is uppercase, 0 otherwise.
  */
-int _isupper(int c);
+int _isupper(int c)
 {
-	if (c >= 'A' && c <= 'z')
+	if (c >= 65  && c <= 90)
 	{
 		return (1);
 	}
